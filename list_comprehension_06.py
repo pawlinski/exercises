2b1d096df65440ea1d8728e9ab638ebfac91b23d
pawlinskiradek <radoslaw.pawlinski@gmail.com>
2024-02-08 11:54:25 +0100

    ex 6 done

--- list_comprehension_06.py
+++ list_comprehension_06.py
@@ -1,2 +1,24 @@
 # list_comprehension_06.py
 # Find the common numbers in two lists (without using a tuple or set) list_a = 1, 2, 3, 4, list_b = 2, 3, 4, 5
+list_a = [1, 2, 3, 4] 
+list_b = [2, 3, 4, 5]
+result = []
+
+
+# for item_a in list_a:
+#     for item_b in list_b:
+#         if item_a == item_b:
+#             result.append(item_a)
+
+# or
+# result = [item_a for item_a in list_a for item_b in list_b if item_a == item_b]
+
+# or
+# for item_a in list_a:
+#     if item_a in list_b:
+#         result.append(item_a)
+
+# or
+result = [item_a for item_a in list_a if item_a in list_b]
+
+print(result)
