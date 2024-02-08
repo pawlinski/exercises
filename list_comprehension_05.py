a5a14fe33d90a24f81189a52afc250263b57f39a
pawlinskiradek <radoslaw.pawlinski@gmail.com>
2024-02-08 11:43:38 +0100

    ex 5 done

--- list_comprehension_05.py
+++ list_comprehension_05.py
@@ -1,2 +1,12 @@
 # list_comprehension_05.py
 # Get the index and the value as a tuple for items in the list “hi”, 4, 8.99, ‘apple’, (‘t,b’,’n’). Result would look like (index, value), (index, value)
+some_list = ["hi", 4, 8.99, 'apple', ('t,b','n')]
+new_list = []
+
+# for index, value in enumerate(some_list):
+#     new_list.append((index, value))
+
+# or
+new_list = [(index, value) for index, value in enumerate(some_list)]
+
+print(new_list)
